d137e74ea52cd7298474a98e628fa760b4ac2d9a
dianGrigorov <diangrigorov@gmail.com>
2022-12-26 16:40:13 +0200

    Update FormCustomers.Designer.cs

--- InventoryManage/Forms/FormCustomers.Designer.cs
+++ InventoryManage/Forms/FormCustomers.Designer.cs
@@ -30,6 +30,9 @@
         {
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
             this.label1 = new System.Windows.Forms.Label();
             this.FbProfileTb = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
@@ -42,6 +45,7 @@
             this.editBtn = new System.Windows.Forms.Button();
             this.removeBtn = new System.Windows.Forms.Button();
             this.dataCustomersGV = new System.Windows.Forms.DataGridView();
+            this.clearBtn = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataCustomersGV)).BeginInit();
             this.SuspendLayout();
             // 
@@ -58,11 +62,12 @@
             // FbProfileTb
             // 
             this.FbProfileTb.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.FbProfileTb.Font = new System.Drawing.Font("Rockwell", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.FbProfileTb.Location = new System.Drawing.Point(199, 5);
+            this.FbProfileTb.Font = new System.Drawing.Font("Arial Unicode MS", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.FbProfileTb.Location = new System.Drawing.Point(199, 6);
             this.FbProfileTb.Name = "FbProfileTb";
-            this.FbProfileTb.Size = new System.Drawing.Size(271, 30);
+            this.FbProfileTb.Size = new System.Drawing.Size(271, 33);
             this.FbProfileTb.TabIndex = 1;
+            this.FbProfileTb.TextChanged += new System.EventHandler(this.FbProfileTb_TextChanged);
             // 
             // label2
             // 
@@ -97,34 +102,34 @@
             // CustomersNameTb
             // 
             this.CustomersNameTb.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.CustomersNameTb.Font = new System.Drawing.Font("Rockwell", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.CustomersNameTb.Location = new System.Drawing.Point(199, 54);
+            this.CustomersNameTb.Font = new System.Drawing.Font("Arial Unicode MS", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.CustomersNameTb.Location = new System.Drawing.Point(199, 55);
             this.CustomersNameTb.Name = "CustomersNameTb";
-            this.CustomersNameTb.Size = new System.Drawing.Size(271, 30);
+            this.CustomersNameTb.Size = new System.Drawing.Size(271, 33);
             this.CustomersNameTb.TabIndex = 5;
             // 
             // CustomersPhoneTb
             // 
             this.CustomersPhoneTb.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.CustomersPhoneTb.Font = new System.Drawing.Font("Rockwell", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.CustomersPhoneTb.Location = new System.Drawing.Point(199, 109);
+            this.CustomersPhoneTb.Font = new System.Drawing.Font("Arial Unicode MS", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.CustomersPhoneTb.Location = new System.Drawing.Point(199, 104);
             this.CustomersPhoneTb.Name = "CustomersPhoneTb";
-            this.CustomersPhoneTb.Size = new System.Drawing.Size(271, 30);
+            this.CustomersPhoneTb.Size = new System.Drawing.Size(271, 33);
             this.CustomersPhoneTb.TabIndex = 6;
             // 
             // CustomersAddressTb
             // 
             this.CustomersAddressTb.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.CustomersAddressTb.Font = new System.Drawing.Font("Rockwell", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.CustomersAddressTb.Font = new System.Drawing.Font("Arial Unicode MS", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.CustomersAddressTb.Location = new System.Drawing.Point(199, 161);
             this.CustomersAddressTb.Multiline = true;
             this.CustomersAddressTb.Name = "CustomersAddressTb";
-            this.CustomersAddressTb.Size = new System.Drawing.Size(271, 202);
+            this.CustomersAddressTb.Size = new System.Drawing.Size(271, 205);
             this.CustomersAddressTb.TabIndex = 7;
             // 
             // addBtn
             // 
-            this.addBtn.Location = new System.Drawing.Point(12, 210);
+            this.addBtn.Location = new System.Drawing.Point(12, 188);
             this.addBtn.Name = "addBtn";
             this.addBtn.Size = new System.Drawing.Size(172, 40);
             this.addBtn.TabIndex = 8;
@@ -134,64 +139,91 @@
             // 
             // editBtn
             // 
-            this.editBtn.Location = new System.Drawing.Point(12, 267);
+            this.editBtn.Location = new System.Drawing.Point(12, 234);
             this.editBtn.Name = "editBtn";
             this.editBtn.Size = new System.Drawing.Size(172, 40);
             this.editBtn.TabIndex = 9;
             this.editBtn.Text = "EDIT";
             this.editBtn.UseVisualStyleBackColor = true;
-            this.editBtn.Click += new System.EventHandler(this.button1_Click);
+            this.editBtn.Click += new System.EventHandler(this.EditBtn_Click);
             // 
             // removeBtn
             // 
-            this.removeBtn.Location = new System.Drawing.Point(12, 326);
+            this.removeBtn.Location = new System.Drawing.Point(12, 280);
             this.removeBtn.Name = "removeBtn";
             this.removeBtn.Size = new System.Drawing.Size(172, 40);
             this.removeBtn.TabIndex = 11;
             this.removeBtn.Text = "DEL";
             this.removeBtn.UseVisualStyleBackColor = true;
+            this.removeBtn.Click += new System.EventHandler(this.removeBtn_Click);
             // 
             // dataCustomersGV
             // 
+            dataGridViewCellStyle1.Padding = new System.Windows.Forms.Padding(1);
+            this.dataCustomersGV.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
             this.dataCustomersGV.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.dataCustomersGV.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
-            this.dataCustomersGV.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataCustomersGV.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataCustomersGV.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
             this.dataCustomersGV.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.dataCustomersGV.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
-            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
-            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle1.Font = new System.Drawing.Font("Rockwell", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dataCustomersGV.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
-            this.dataCustomersGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataCustomersGV.Location = new System.Drawing.Point(476, 5);
-            this.dataCustomersGV.Name = "dataCustomersGV";
-            this.dataCustomersGV.ReadOnly = true;
-            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            this.dataCustomersGV.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
             dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle2.Font = new System.Drawing.Font("Rockwell", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Rockwell", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
             dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
             dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
             dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dataCustomersGV.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dataCustomersGV.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dataCustomersGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataCustomersGV.DefaultCellStyle = dataGridViewCellStyle3;
+            this.dataCustomersGV.Location = new System.Drawing.Point(476, 6);
+            this.dataCustomersGV.Name = "dataCustomersGV";
+            this.dataCustomersGV.ReadOnly = true;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Rockwell", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataCustomersGV.RowHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            this.dataCustomersGV.RowHeadersVisible = false;
+            this.dataCustomersGV.RowHeadersWidth = 51;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Arial Unicode MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.dataCustomersGV.RowsDefaultCellStyle = dataGridViewCellStyle5;
+            this.dataCustomersGV.RowTemplate.DefaultCellStyle.Font = new System.Drawing.Font("Arial Unicode MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.dataCustomersGV.RowTemplate.ReadOnly = true;
             this.dataCustomersGV.RowTemplate.Resizable = System.Windows.Forms.DataGridViewTriState.True;
             this.dataCustomersGV.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataCustomersGV.Size = new System.Drawing.Size(390, 361);
+            this.dataCustomersGV.Size = new System.Drawing.Size(320, 360);
             this.dataCustomersGV.TabIndex = 12;
             this.dataCustomersGV.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataCustomersGV_CellContentClick);
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Location = new System.Drawing.Point(12, 326);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(172, 40);
+            this.clearBtn.TabIndex = 13;
+            this.clearBtn.Text = "CLEAR";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
             // 
             // FormCustomers
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.clearBtn);
             this.Controls.Add(this.dataCustomersGV);
             this.Controls.Add(this.removeBtn);
             this.Controls.Add(this.editBtn);
@@ -227,5 +259,6 @@
         private System.Windows.Forms.Button editBtn;
         private System.Windows.Forms.Button removeBtn;
         private System.Windows.Forms.DataGridView dataCustomersGV;
+        private System.Windows.Forms.Button clearBtn;
     }
 }
